6473890a805ce79956cfb7e544ccc46218f31e3e
Evandro Gonçalves <84279332+underline84@users.noreply.github.com>
2022-07-14 15:40:51 -0300

    first commit

---
+++ src/components/ListaDeEventos/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import useListaDeEvento from '../../state/hooks/useListaDeEventos';
+import Evento from '../Evento';
+import Filtro from '../Filtro';
+import style from './ListaDeEventos.module.scss';
+
+const ListaDeEventos: React.FC = () => {
+
+  const eventos = useListaDeEvento();
+
+  return (<section>
+    <Filtro/>
+    <div className={style.Scroll}>
+      {eventos.map(evento => (
+        <Evento evento={evento} key={evento.id} />
+      ))}
+    </div>
+  </section>)
+}
+
+export default ListaDeEventos
